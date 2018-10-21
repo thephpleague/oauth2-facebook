4ad45687d2f33ef17ad57060d191954a1ade44c3
Simon <perdrisat@gmail.com>
2018-10-21 19:42:11 +0200

    Fix #39 - compare graphAPI version correctly

--- src/Provider/Facebook.php
+++ src/Provider/Facebook.php
@@ -107,7 +107,7 @@ class Facebook extends AbstractProvider
         ];
 
         // backwards compatibility less than 2.8
-        if (version_compare(substr($this->graphApiVersion, 1), '2.8') < 0) {
+        if (version_compare($this->graphApiVersion , '2.8', '>')) {
             $fields[] = 'bio';
         }
 
