2459d6dffb06e235d96ab816729f1e63517d2709
Mike Almond <mike.almond@gmail.com>
2020-07-20 11:08:08 -0400

    Remove cover and timezone from the Facebook user tests

--- tests/src/Provider/FacebookUserTest.php
+++ tests/src/Provider/FacebookUserTest.php
@@ -16,11 +16,9 @@ class FacebookUserTest extends \PHPUnit_Framework_TestCase
         $this->user = new FacebookUser([
             'id' => '4',
             'picture' => ['data' => ['is_silhouette' => true, 'url' => 'foo.com/pic.jpg']],
-            'cover' => ['id' => '123', 'source' => 'foo.com/cover.jpg'],
             'first_name' => 'Mark',
             'last_name' => 'Zuck',
             'foo' => 'bar',
-            'timezone' => '-8',
             'age_range' => ['min' => 21],
         ]);
     }
@@ -50,14 +48,11 @@ class FacebookUserTest extends \PHPUnit_Framework_TestCase
         $expectedData = [
           'id' => '4',
           'picture' => ['data' => ['is_silhouette' => true, 'url' => 'foo.com/pic.jpg']],
-          'cover' => ['id' => '123', 'source' => 'foo.com/cover.jpg'],
           'first_name' => 'Mark',
           'last_name' => 'Zuck',
           'foo' => 'bar',
           'picture_url' => 'foo.com/pic.jpg',
           'is_silhouette' => true,
-          'cover_photo_url' => 'foo.com/cover.jpg',
-          'timezone' => '-8',
           'age_range' => ['min' => 21],
         ];
 
