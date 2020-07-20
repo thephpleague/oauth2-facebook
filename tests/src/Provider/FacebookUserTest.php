f552660d3451850ca2d152f9db7594c6df8d2421
Mike Almond <mike.almond@gmail.com>
2020-07-20 11:09:57 -0400

    Update FacebookUserTest.php

--- tests/src/Provider/FacebookUserTest.php
+++ tests/src/Provider/FacebookUserTest.php
@@ -38,7 +38,6 @@ class FacebookUserTest extends \PHPUnit_Framework_TestCase
     public function testProperlyMutatesPhotoUrls()
     {
         $this->assertEquals('foo.com/pic.jpg', $this->user->getPictureUrl());
-        $this->assertEquals('foo.com/cover.jpg', $this->user->getCoverPhotoUrl());
     }
 
     public function testCanGetAllDataBackAsAnArray()
